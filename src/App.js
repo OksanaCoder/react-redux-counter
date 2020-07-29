d9d042c10b25b00d450872e9a1e7bd6fa9f39eed
Oksana <myzove@mail.ru>
2020-07-30 01:54:51 +0300

    adding connect

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { connect } from 'react-redux'
 
-function App() {
+const increment = (dispatch) => {
+  dispatch({
+    type: 'INCREMENT'
+  })
+}
+
+const decrement = (dispatch) => {
+  dispatch({
+    type: 'DECREMENT'
+  })
+}
+const mapStateToProps = (state) => ({ ... state})
+function App(props) {
   return (
-    <div className="App">
-      
+    <div className="container">
+        <div>
+          <button className='increment'>-</button> 
+              {props.counter}
+          <button className='decrement'>+</button>
+        </div>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
